03eb22f3cc828333f75bd9f795b7ea93749974c2
jhonned01 <jhonned01@gmail.com>
2024-09-27 11:09:41 -0500

    Listo Granja linda falta pulir un par de detalles

---
+++ app/Terminos/page.js
@@ -0,0 +1,25 @@
+"use client";
+
+import React from "react";
+import Title from "../Title";
+
+const Terminos = () => {
+  return (
+    <div className="  bg-[#eaeaea]  mb-10">
+      <Title title={"TÉRMINOS Y CONDICIONES "} image="/One.webp" />
+
+      <div className="container mx-auto ">
+        <div className=" flex justify-center items-center  p-1 lg:p-7 gap-x-10">
+          <div className="w-full h-full  rounded-lg ">
+            <iframe
+              src="/TÉRMINOS-Y-CONDICIONES-GRANJA-LINDA-BORRADOR.pdf"
+              className="w-full h-[82vh] rounded-lg"
+            ></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Terminos;
